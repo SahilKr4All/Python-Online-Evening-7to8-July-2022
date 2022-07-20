8bc42a89fe2904111e367e5a92254e91eae9536a
Sahil Kumar <101191455+SahilKr4All@users.noreply.github.com>
2022-07-20 19:57:46 +0530

    Add files via upload

---
+++ Python Operators Console.py
@@ -0,0 +1,47 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x = 5
+>>> x 1 = 5
+SyntaxError: invalid syntax
+>>> x$@1 = 12
+SyntaxError: invalid syntax
+>>> x_1 = 12
+>>> x1=12
+>>> 1x = 12
+SyntaxError: invalid syntax
+>>> 
+>>> x = 12
+>>> type(x)
+<class 'int'>
+>>> x = 12.23424
+>>> type(x)
+<class 'float'>
+>>> x = 1+5j
+>>> type(x)
+<class 'complex'>
+>>> 
+>>> x = 10
+>>> y = 5
+>>> x+y
+15
+>>> x-y
+5
+>>> x*y
+50
+>>> x/y
+2.0
+>>> 22/7
+3.142857142857143
+>>> 22//7
+3
+>>> 12/7
+1.7142857142857142
+>>> 12//7
+1
+>>> 27%7
+6
+>>> 29%3
+2
+>>> 2**3
+8
+>>> 
