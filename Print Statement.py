670ea5c1e1035c2000b76f7a9fd84d9c07f9d610
Sahil Kumar <101191455+SahilKr4All@users.noreply.github.com>
2022-07-21 20:02:02 +0530

    Add files via upload

---
+++ Print Statement.py
@@ -0,0 +1,6 @@
+x = float(input("Enter no 1=>"))
+y = float(input("Enter no 2=>"))
+result = x+y
+print("The Sum of ",x," and ",y," = ",result)
+print("The SUm of {} and {} is {}".format(x,y,result))
+print(f"The SUm of {x} and {y} is {result}")
