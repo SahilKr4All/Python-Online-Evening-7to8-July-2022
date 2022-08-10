773baeec6f7372d495eda728f0e647bab50f2483
Sahil Kumar <101191455+SahilKr4All@users.noreply.github.com>
2022-08-10 19:52:36 +0530

    Add files via upload

--- Recursive function.py
+++ Recursive function.py
@@ -9,6 +9,7 @@ def f1(x):
     print(x)
 f1(1)
 '''
+'''
 def f1(x,y):
     #base case
     if y==0:
@@ -18,3 +19,124 @@ def f1(x,y):
     return res
 res=f1(2,3)
 print(res)
+'''
+'''
+result = 0
+def f1(x):
+
+    if x==10:
+        return 10
+    return x+ f1(x+1)
+
+res=f1(1)
+print(res)
+'''
+'''
+*
+**
+***
+****
+*****
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print("*",end="")
+    print()
+'''
+'''
+1
+22
+333
+4444
+55555
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(i,end="")
+    print()
+
+'''
+
+'''
+1
+12
+123
+1234
+12345
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j,end="")
+    print()
+'''
+'''
+1
+23
+456
+78910
+'''
+'''
+x=1
+for i in range(1,5):
+    for j in range(1,i+1):
+        print(x,end="")
+        x+=1
+    print()
+'''
+'''
+
+*****
+*   *
+*   *
+*   *
+*   *
+*   *
+*****
+
+
+for i in range(1,6):
+    for j in range(1,6):
+        if i==1 or j==1 or i==5 or j==5:
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
+
+
+'''
+'''
+
+*   *
+*   *
+*   *
+*****
+*   *
+*   *
+*   *
+
+
+*    *
+* *  *
+*  * *
+*   **
+*    *
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
