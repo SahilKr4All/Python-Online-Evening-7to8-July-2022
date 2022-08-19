30ed55b746fe74a0d5bf008c35c4b1b3e5bb6bfa
Sahil Kumar <101191455+SahilKr4All@users.noreply.github.com>
2022-08-19 17:47:14 +0530

    Add files via upload

--- Brick Breaker.py
+++ Brick Breaker.py
@@ -8,22 +8,28 @@ black = 0,0,0
 blue = 0,0,255
 brick_w = 80
 brick_h=25
-player_w=150
-player_h=15
-player_x=W/2 - player_w/2
-player_y=H-player_h-10
+bar_w=150
+bar_h=15
+bar_x=W/2 - bar_w/2
+bar_y=H-bar_h-10
 ball_r=10
-ball_x=player_x+player_w/2+ball_r/2
-ball_y=H-player_h
+ball_x=bar_x+bar_w/2+ball_r/2
+ball_y=H-bar_h
 
 move_x=0
-move_y=0
+
+move_ball_x=0
+move_ball_y=0
 brick_list=[]
 
-for i in range(0,12):
-    for j in range(0,10):
-        brick_list.append(pygame.rect(i*100,j*30,brick_w,brick_h))
-        
+rows = 5
+cols = W//brick_w
+
+for i in range(rows):
+    for j in range(cols):
+        rect = pygame.Rect(j*(brick_w+5),i*(brick_h+5),brick_w,brick_h)
+        brick_list.append(rect)
+
 while True:
     for event in pygame.event.get():
         
@@ -33,12 +39,14 @@ while True:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                move_x=-10
+                move_x=-1
             elif event.key == pygame.K_RIGHT:
-                move_x=10
+                move_x=1
             elif event.key == pygame.K_SPACE:
-                move_y = -10
-            
+                move_ball_y = -1
+        else:
+            move_x=0
+
 
      
     gameBoard.fill(white)
@@ -46,23 +54,35 @@ while True:
           
     for i in range(len(brick_list)):
             color = random.randint(0,255),random.randint(0,255),random.randint(0,255)
-            pygame.draw(gameBoard,color,brick_list[i])
+            pygame.draw.rect(gameBoard,color,brick_list[i])
             
-    rect = pygame.draw.rect(gameBoard,black,(player_x,player_y,player_w,player_h))
-    ball = pygame.draw.circle(gameBoard,blue,(ball_x,ball_y-player_h),ball_r)
-
+    rect = pygame.draw.rect(gameBoard,black,(bar_x,bar_y,bar_w,bar_h))
+    ball=pygame.draw.circle(gameBoard,blue,(ball_x,ball_y-bar_h),ball_r)
+            
     for i in range(len(brick_list)):
-        if pygame.rect(brick_list[i]).collide_rect(ball):
-            print("Collision")
-    
+        if brick_list[i].colliderect(ball):
+            del brick_list[i]
+            move_ball_y=1
+            break
 
-    player_x+=move_x
+    bar_x+=move_x
     ball_x+=move_x
 
-    ball_y+=move_y
+    ball_y+=move_ball_y
 
-    if player_x > W-player_w:
+    
+
+    if bar_x > W-bar_w:
         move_x=-1
-    elif player_x<0:
+    elif bar_x<0:
         move_x=1
+
+    if rect.colliderect(ball):
+        move_ball_y=-1
+    elif ball_y<0:
+        move_ball_y=1
+    elif ball_y>H+ball_r:
+        ball_x=bar_x+bar_w/2+ball_r/2
+        ball_y=H-bar_h
+    
     pygame.display.update()
